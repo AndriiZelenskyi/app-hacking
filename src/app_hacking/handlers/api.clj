adf92838928eb11403c181aa899ed70df0d7142d
BogdanOros <orossbogdan@gmail.com>
2021-12-11 19:46:18 +0200

    add space

--- src/app_hacking/handlers/api.clj
+++ src/app_hacking/handlers/api.clj
@@ -5,6 +5,7 @@
   {:time (System/currentTimeMillis)
    :req (merge req {:async-channel nil})})
 
+
 (defn get-health [req]
   {:status (if (db-healthy?) 200 500)
    :body (if (db-healthy?) "UP" "DOWN")})
